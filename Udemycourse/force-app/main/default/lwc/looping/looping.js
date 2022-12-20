7bc07002f73c807d33c2682c58c4a0e430fb28ba
Skelber <56479422+Skelber@users.noreply.github.com>
2022-12-20 21:58:36 +0100

    Add files via upload

---
+++ Udemycourse/force-app/main/default/lwc/looping/looping.js
@@ -0,0 +1,28 @@
+import { LightningElement } from 'lwc';
+
+export default class Looping extends LightningElement {
+    carList = ["Ford", "Audi", "BMW", "Hyundai", "Opel"]
+
+    ceoList = [
+        {
+            id:1,
+            company:"Google",
+            name:"Sundar Pichai"
+        },
+        {
+            id:2,
+            company:"Apple",
+            name:"Tim Cook"
+        },
+        {
+            id:3,
+            company:"Facebook",
+            name:"Mark Zuckerberg"
+        },
+        {
+            id:4,
+            company:"Amazon",
+            name:"Jeff Bezos"
+        },
+    ]
+}
