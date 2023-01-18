e74b4198bad5beee4da31a259a520e4e15b76771
Skelber <kelber11@hotmail.com>
2023-01-18 16:50:02 +0100

    Parent to child pt 2

--- Udemy/force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
+++ Udemy/force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
@@ -19,5 +19,14 @@ export default class P2cParentComponent extends LightningElement {
     }
     
   ]
+
+  percentage=10
+  changeHandler(event){
+    this.percentage = event.target.value
+  }
+
+  handleClick(){
+    this.template.querySelector('c-p2c-slider-component').resetSlider()
+  }
 }
 
---
+++ Udemy/force-app/main/default/lwc/p2cProgressComonent/p2cProgressComonent.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc';
+
+export default class P2cProgressComonent extends LightningElement {
+    @api progressValue
+}
