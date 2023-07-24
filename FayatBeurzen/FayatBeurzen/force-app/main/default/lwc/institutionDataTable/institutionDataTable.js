df12ebdf67a085d5dd8f1a377c99f727ee0dccf1
Skelber <kelber11@hotmail.com>
2023-07-24 16:58:42 +0200

    Update the LWC component

--- FayatBeurzen/FayatBeurzen/force-app/main/default/lwc/institutionDataTable/institutionDataTable.js
+++ FayatBeurzen/FayatBeurzen/force-app/main/default/lwc/institutionDataTable/institutionDataTable.js
@@ -1,11 +1,11 @@
-import { LightningElement, track, wire } from 'lwc';
-import { getObjectInfo } from 'lightning/uiObjectInfoApi';
-import PREFERRED_EDUCATION_OBJECT from '@salesforce/schema/Preferred_education__c';
-import PREFERRED_EDUCATION_DOMAIN_FIELD from '@salesforce/schema/Preferred_education__c.Preferred_Education_Domain__c';
+import { LightningElement, api, track, wire } from 'lwc';
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
+import PREFERRED_EDUCATION_DOMAIN_FIELD from '@salesforce/schema/Preferred_Education__c.Preferred_Education_Domain__c';
 
 export default class MyComponent extends LightningElement {
     @track preferredEducation;
-    defaultRecordTypeId;
+    defaultRecordTypeId; 
+
     const
      columns = [
         { label: 'Education', fieldName: 'label' },
@@ -17,11 +17,11 @@ export default class MyComponent extends LightningElement {
 }
 
     
-    @wire(getObjectInfo, { objectApiName:PREFERRED_EDUCATION_OBJECT, recordId:'a1m7E000001llHuQAI' ,  recordTypeid: '$defaultRecordTypeId', fieldApiName: PREFERRED_EDUCATION_DOMAIN_FIELD })
+    @wire(getPicklistValues, { recordTypeId: '01I7E000002PfJp', fieldApiName: PREFERRED_EDUCATION_DOMAIN_FIELD})
 
-    objectInfo({ error, data }) {
+    picklistValues({ error, data }) {
         if (data) {
-            const picklistValues = data.fields.Preferred_education__c.Preferred_Education_Domain__c.picklistValues;
+            const picklistValues = picklistValues;
             this.preferredEducation = picklistValues.map(picklistValue => ({
                 label: picklistValue.label,
                 value: picklistValue.value
