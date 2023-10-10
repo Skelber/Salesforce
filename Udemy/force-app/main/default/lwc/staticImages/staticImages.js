0e5a4cc94de1cd3c4f228a0e7d6daa725746166e
Skelber <kelber11@hotmail.com>
2023-10-10 14:57:11 +0200

    static resources and third party files pt1

---
+++ Udemy/force-app/main/default/lwc/staticImages/staticImages.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import DOG_IMAGE from '@salesforce/resourceUrl/dog_image';
+
+export default class StaticImages extends LightningElement {
+    dogImage = DOG_IMAGE
+}
