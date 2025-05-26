0298fe9b596c729dfc37b8cbbef67032136f9df5
Steven Kelber <stevenkelber@Stevens-MacBook-Air.local>
2025-05-26 23:10:23 +0200

    transformed worktypes coming from Apex in the right order and showed them on the layout

--- Vigodev/force-app/main/default/lwc/selectWorktype/selectWorktype.js
+++ Vigodev/force-app/main/default/lwc/selectWorktype/selectWorktype.js
@@ -1,9 +1,52 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import getWorkType from '@salesforce/apex/WorktypeSelection.getJSON';
 import bandageIcon from "@salesforce/resourceUrl/bandage";
 import prostheticIcon from "@salesforce/resourceUrl/prosthetic";
 import orthoticIcon from "@salesforce/resourceUrl/orthotics";
 
 export default class SelectWorktype extends LightningElement {
+
+   workTypes = []
+   productGroups = [];
+
+    connectedCallback(){
+        // console.log('Worktypes ' + JSON.stringify(this.workTypes))
+        this.getWorkTypes();
+    }
+
+    getWorkTypes() {
+        getWorkType()
+          .then(result => {
+            // 1) If it’s a JSON string, parse it
+            if (typeof result === 'string') {
+              try {
+                result = JSON.parse(result);
+              } catch (e) {
+                result = {}; 
+              }
+            }
+    
+            if (result.workTypeSelector) {
+              this.workTypes = Array.from(result.workTypeSelector);
+            }
+            else if (Array.isArray(result)) {
+              this.workTypes = Array.from(result);
+            }
+            
+          })
+          .catch(error => {
+            console.error('Error in getWorkTypes:', error);
+          });
+
+          let i = 0;
+          this.workTypes.forEach((businessUnit) =>
+            console.log('bu' + businessUnit))
+
+    }
+
+    handleBUClick(event){
+        console.log('clicked ' + JSON.stringify(event.target.value))
+    }
 
     bandageIcon = bandageIcon;
     prostheticIcon = prostheticIcon;
@@ -23,4 +66,5 @@ export default class SelectWorktype extends LightningElement {
     handleSectionHeaderClick(event){
         this.activeSection = event.target.value
     }
+    
 }
